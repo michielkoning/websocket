194d16e53ab67ec243bb15db31f572a271c1905e
MIchiel Koning <michiel.koning@valtech.com>
2024-03-14 15:05:41 +0100

    ws

--- backend/index.js
+++ backend/index.js
@@ -7,16 +7,10 @@ wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
-    console.log("received: %s", data);
-    ws.send("tes22");
-
-    ws.on("message", function message(data) {
-      console.log(data);
-      wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data.toString());
-        }
-      });
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data.toString());
+      }
     });
   });
 });
